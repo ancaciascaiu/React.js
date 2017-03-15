8f411acdf1e3dc66b90c75502c27d023093d0d6d
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2017-03-14 19:44:30 -0500

    handle state for appointments component, because it contains AppointmentForm and AppointmentList

--- calreact/app/assets/javascripts/components/appointments.jsx
+++ calreact/app/assets/javascripts/components/appointments.jsx
@@ -1,13 +1,23 @@
 var Appointments = React.createClass({
+	getInitialState: function(){
+		return{
+			appointments: this.props.appointments,
+			title: 'Food',
+			appt_time: 'Tomorrow at 9am'
+		}
+	},
+
+	handleUserInput: function(obj){
+		this.setState(obj);
+	},
+
 	render: function(){
 		return(
 			<div>
-				<AppointmentForm />
-				{this.props.appointments.map(function(appointment){
-					return(
-						<Appointment appointment={appointment} />
-						)
-				})}
+				<AppointmentForm input_title={this.state.title} 
+					input_appt_time={this.state.appt_time} 
+					onUserInput={this.handleUserInput} />
+				<AppointmentsList appointments={this.state.appointments} />
 			</div>
 			)
 	}
